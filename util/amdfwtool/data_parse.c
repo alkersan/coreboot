alkersan/coreboot

commit c5e28abaf803465ae4bfec1904618497e077ca50

amdfwtool: Take a config file instead of command line parameters
To verify the consistency, see if timeless builds with and without
this patch result in identical coreboot.rom files.

BUG=b:154032833
TEST=Build & boot on mandolin

Change-Id: Icae73d0730106aab687486e555ba947796e5e757
Signed-off-by: Zheng Bao <fishbaozi@gmail.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/42859
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Nikolai Vyssotski <nikolai.vyssotski@amd.corp-partner.google.com>
Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

util/amdfwtool/data_parse.c
@@ -1,0 +1,418 @@
+#include <stdio.h>
+#include <regex.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "amdfwtool.h"
+
+/* TODO: a empty line does not matched. */
+static const char blank_or_comment_regex[] =
+	/* a blank line */
+	"(^[[:space:]]*$)"
+	"|"	/* or ... */
+	/* a line consisting of: optional whitespace followed by */
+	"(^[[:space:]]*"
+	/* a '#' character and optionally, additional characters */
+	"#.*$)";
+static regex_t blank_or_comment_expr;
+
+static const char entries_line_regex[] =
+	/* optional whitespace */
+	"^[[:space:]]*"
+	/* followed by a chunk of nonwhitespace for macro field */
+	"([^[:space:]]+)"
+	/* followed by one or more whitespace characters */
+	"[[:space:]]+"
+	/* followed by a chunk of nonwhitespace for filename field */
+	"([^[:space:]]+)"
+	/* followed by optional whitespace */
+	"[[:space:]]*$";
+static regex_t entries_line_expr;
+
+void compile_reg_expr(int cflags, const char *expr, regex_t *reg)
+{
+	static const size_t ERROR_BUF_SIZE = 256;
+	char error_msg[ERROR_BUF_SIZE];
+	int result;
+
+	result = regcomp(reg, expr, cflags);
+	if (result != 0) {
+		regerror(result, reg, error_msg, ERROR_BUF_SIZE);
+		printf("%s\n", error_msg);
+	}
+}
+
+extern amd_fw_entry amd_psp_fw_table[];
+extern amd_bios_entry amd_bios_table[];
+
+static uint8_t find_register_fw_filename_psp_dir(char *fw_name, char *filename,
+		amd_cb_config *cb_config)
+{
+	amd_fw_type fw_type = AMD_FW_INVALID;
+	amd_fw_entry *psp_tableptr;
+	uint8_t subprog;
+
+	if (strcmp(fw_name, "PSPBTLDR_WL_FILE") == 0) {
+		if (cb_config->have_whitelist == 1) {
+			fw_type = AMD_FW_PSP_BOOTLOADER;
+			subprog = 0;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else if (strcmp(fw_name, "PSPBTLDR_FILE") == 0) {
+		if (cb_config->have_whitelist == 0) {
+			fw_type = AMD_FW_PSP_BOOTLOADER;
+			subprog = 0;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else if (strcmp(fw_name, "AMD_PUBKEY_FILE") == 0) {
+		fw_type = AMD_FW_PSP_PUBKEY;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSPRCVR_FILE") == 0) {
+		fw_type = AMD_FW_PSP_RECOVERY;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PUBSIGNEDKEY_FILE") == 0) {
+		fw_type = AMD_FW_PSP_RTM_PUBKEY;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSPNVRAM_FILE") == 0) {
+		fw_type = AMD_FW_PSP_NVRAM;
+		subprog = 0;
+	} else if (strcmp(fw_name, "SMUSCS_FILE") == 0) {
+		fw_type = AMD_FW_PSP_SMUSCS;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSPTRUSTLETS_FILE") == 0) {
+		fw_type = AMD_FW_PSP_TRUSTLETS;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSPSECUREDEBUG_FILE") == 0) {
+		fw_type = AMD_FW_PSP_SECURED_DEBUG;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_SMUFW1_SUB0_FILE") == 0) {
+		fw_type = AMD_FW_PSP_SMU_FIRMWARE;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_SMUFW1_SUB1_FILE") == 0) {
+		fw_type = AMD_FW_PSP_SMU_FIRMWARE;
+		subprog = 1;
+	} else if (strcmp(fw_name, "PSP_SMUFW1_SUB2_FILE") == 0) {
+		fw_type = AMD_FW_PSP_SMU_FIRMWARE;
+		subprog = 2;
+	} else if (strcmp(fw_name, "PSP_SMUFW2_SUB0_FILE") == 0) {
+		fw_type = AMD_FW_PSP_SMU_FIRMWARE2;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_SMUFW2_SUB1_FILE") == 0) {
+		fw_type = AMD_FW_PSP_SMU_FIRMWARE2;
+		subprog = 1;
+	} else if (strcmp(fw_name, "PSP_SMUFW2_SUB2_FILE") == 0) {
+		fw_type = AMD_FW_PSP_SMU_FIRMWARE2;
+		subprog = 2;
+	} else if (strcmp(fw_name, "PSP_SEC_DBG_KEY_FILE") == 0) {
+		if (cb_config->unlock_secure == 1) {
+			fw_type = AMD_FW_PSP_SECURED_DEBUG;
+			subprog = 0;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else if (strcmp(fw_name, "PSP_SEC_DEBUG_FILE") == 0) {
+		if (cb_config->unlock_secure == 1) {
+			fw_type = AMD_DEBUG_UNLOCK;
+			subprog = 0;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else if (strcmp(fw_name, "PSP_ABL0_FILE") == 0) {
+		fw_type = AMD_ABL0;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_ABL1_FILE") == 0) {
+		fw_type = AMD_ABL1;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_ABL2_FILE") == 0) {
+		fw_type = AMD_ABL2;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_ABL3_FILE") == 0) {
+		fw_type = AMD_ABL3;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_ABL4_FILE") == 0) {
+		fw_type = AMD_ABL4;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_ABL5_FILE") == 0) {
+		fw_type = AMD_ABL5;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_ABL6_FILE") == 0) {
+		fw_type = AMD_ABL6;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_ABL7_FILE") == 0) {
+		fw_type = AMD_ABL7;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSPSECUREOS_FILE") == 0) {
+		if (cb_config->use_secureos == 1) {
+			fw_type = AMD_FW_PSP_SECURED_OS;
+			subprog = 0;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else if (strcmp(fw_name, "PSPTRUSTLETS_FILE") == 0) {
+		if (cb_config->use_secureos) {
+			fw_type = AMD_FW_PSP_TRUSTLETS;
+			subprog = 0;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else if (strcmp(fw_name, "TRUSTLETKEY_FILE") == 0) {
+		if (cb_config->use_secureos) {
+			fw_type = AMD_FW_PSP_TRUSTLETKEY;
+			subprog = 0;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else if (strcmp(fw_name, "PSP_IKEK_FILE") == 0) {
+		fw_type = AMD_WRAPPED_IKEK;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_SECG1_FILE") == 0) {
+		fw_type = AMD_SEC_GASKET;
+		subprog = 1;
+	} else if (strcmp(fw_name, "PSP_SECG2_FILE") == 0) {
+		fw_type = AMD_SEC_GASKET;
+		subprog = 2;
+	} else if (strcmp(fw_name, "PSP_MP2FW1_FILE") == 0) {
+		if (cb_config->load_mp2_fw == 1) {
+			fw_type = AMD_MP2_FW;
+			subprog = 1;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else if (strcmp(fw_name, "PSP_MP2FW2_FILE") == 0) {
+		if (cb_config->load_mp2_fw == 1) {
+			fw_type = AMD_MP2_FW;
+			subprog = 2;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else if (strcmp(fw_name, "PSP_MP2CFG_FILE") == 0) {
+		if (cb_config->load_mp2_fw == 1) {
+			fw_type = AMD_BIOS_MP2_CFG;
+			subprog = 0;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else if (strcmp(fw_name, "PSP_DRIVERS_FILE") == 0) {
+		fw_type = AMD_DRIVER_ENTRIES;
+		subprog = 0;
+	} else if (strcmp(fw_name, "PSP_S0I3_FILE") == 0) {
+		if (cb_config->s0i3 == 1) {
+			fw_type = AMD_S0I3_DRIVER;
+			subprog = 0;
+		} else {
+			fw_type = AMD_FW_SKIP;
+		}
+	} else {
+		fw_type = AMD_FW_INVALID;
+		/* TODO: Add more */
+	}
+	/* Search and fill the filename */
+	psp_tableptr = &amd_psp_fw_table[0];
+	if (fw_type != AMD_FW_SKIP && fw_type != AMD_FW_INVALID) {
+		while (psp_tableptr->type != AMD_FW_INVALID) {
+			/* instance are not used in PSP table */
+			if (psp_tableptr->type == fw_type && psp_tableptr->subprog == subprog) {
+				psp_tableptr->filename = filename;
+				break;
+			}
+			psp_tableptr++;
+		}
+	}
+	if (fw_type == AMD_FW_INVALID)
+		return 0;
+	else
+		return 1;
+}
+
+static uint8_t find_register_fw_filename_bios_dir(char *fw_name, char *filename,
+		amd_cb_config *cb_config)
+{
+	amd_bios_type fw_type = AMD_BIOS_INVALID;
+	amd_bios_entry *bhd_tableptr;
+	uint8_t subprog, instance = 0;
+
+	(void) (cb_config);	/* Remove warning and reserved for future. */
+
+	if (strcmp(fw_name, "PSP_PMUI_FILE1") == 0) {
+		fw_type = AMD_BIOS_PMUI;
+		subprog = 0;
+		instance = 1;
+	} else if (strcmp(fw_name, "PSP_PMUI_FILE2") == 0) {
+		fw_type = AMD_BIOS_PMUI;
+		subprog = 0;
+		instance = 4;
+	} else if (strcmp(fw_name, "PSP_PMUI_FILE3") == 0) {
+		fw_type = AMD_BIOS_PMUI;
+		subprog = 1;
+		instance = 1;
+	} else if (strcmp(fw_name, "PSP_PMUI_FILE4") == 0) {
+		fw_type = AMD_BIOS_PMUI;
+		subprog = 1;
+		instance = 4;
+	} else if (strcmp(fw_name, "PSP_PMUD_FILE1") == 0) {
+		fw_type = AMD_BIOS_PMUD;
+		subprog = 0;
+		instance = 1;
+	} else if (strcmp(fw_name, "PSP_PMUD_FILE2") == 0) {
+		fw_type = AMD_BIOS_PMUD;
+		subprog = 0;
+		instance = 4;
+	} else if (strcmp(fw_name, "PSP_PMUD_FILE3") == 0) {
+		fw_type = AMD_BIOS_PMUD;
+		subprog = 1;
+		instance = 1;
+	} else if (strcmp(fw_name, "PSP_PMUD_FILE4") == 0) {
+		fw_type = AMD_BIOS_PMUD;
+		subprog = 1;
+		instance = 4;
+	} else {
+		fw_type = AMD_BIOS_INVALID;
+	}
+
+	bhd_tableptr = amd_bios_table;
+
+	if (fw_type != AMD_BIOS_INVALID && fw_type != AMD_BIOS_SKIP) {
+		while (bhd_tableptr->type != AMD_BIOS_INVALID) {
+			if (bhd_tableptr->type == fw_type &&
+					bhd_tableptr->subpr == subprog &&
+					bhd_tableptr->inst  == instance) {
+				bhd_tableptr->filename = filename;
+				break;
+			}
+			bhd_tableptr++;
+		}
+	}
+	if (fw_type == AMD_BIOS_INVALID)
+		return 0;
+	else
+		return 1;
+}
+
+#define MAX_LINE_SIZE 1024
+
+int get_input_file_line(FILE *f, char line[], int line_buf_size)
+{
+	if (fgets(line, line_buf_size, f) == NULL)
+		return LINE_EOF;
+
+	/* If the file contains a line that is too long, then it's best
+	 * to let the user know right away rather than passing back a
+	 * truncated result that will lead to problems later on.
+	 */
+	line[strlen(line) - 1] = '\0';
+
+	if (strlen(line) == ((size_t) (line_buf_size - 1))) {
+		printf("The line size in config file should be lower than %d bytes.\n",
+			MAX_LINE_SIZE);
+		exit(1);
+	}
+
+	return OK;
+}
+
+static int is_valid_entry(char *oneline, regmatch_t *match)
+{
+	int retval;
+
+	if (regexec(&entries_line_expr, oneline, 3, match, 0) == 0) {
+		oneline[match[1].rm_eo] = '\0';
+		oneline[match[2].rm_eo] = '\0';
+		retval = 1;
+	} else
+		retval = 0;
+
+	return retval;
+}
+
+static int skip_comment_blank_line(char *oneline)
+{
+	int retval;
+
+	if (regexec(&blank_or_comment_expr, oneline, 0, NULL, 0) == 0) {
+		/* skip comment and blank */
+		retval = 1;
+	} else {
+		/* no match */
+		retval = 0;
+	}
+
+	return retval;
+}
+
+#define N_MATCHES 4
+/*
+  return value:
+	0: The config file can not be parsed correctly.
+	1: The config file can be parsed correctly.
+ */
+uint8_t process_config(FILE *config, amd_cb_config *cb_config, uint8_t print_deps)
+{
+	char oneline[MAX_LINE_SIZE], *path_filename;
+	regmatch_t match[N_MATCHES];
+	char dir[MAX_LINE_SIZE] = {'\0'};
+
+	compile_reg_expr(REG_EXTENDED | REG_NEWLINE,
+		blank_or_comment_regex, &blank_or_comment_expr);
+	compile_reg_expr(REG_EXTENDED | REG_NEWLINE,
+		entries_line_regex, &entries_line_expr);
+
+	/* Get a line */
+	/* Get FIRMWARE_LOCATE in the first loop */
+	while (get_input_file_line(config, oneline, MAX_LINE_SIZE) == OK) {
+		/* get a line */
+		if (skip_comment_blank_line(oneline))
+			continue;
+		if (is_valid_entry(oneline, match)) {
+			if (strcmp(&(oneline[match[1].rm_so]), "FIRMWARE_LOCATE") == 0) {
+				strcpy(dir, &(oneline[match[2].rm_so]));
+				break;
+			}
+		}
+	}
+
+	if (dir[0] == '\0') {
+		fprintf(stderr, "No line with FIRMWARE_LOCATION\n");
+		return 0;
+	}
+
+	fseek(config, 0, SEEK_SET);
+	/* Get a line */
+	while (get_input_file_line(config, oneline, MAX_LINE_SIZE) == OK) {
+		/* get a line */
+		if (skip_comment_blank_line(oneline))
+			continue;
+		if (is_valid_entry(oneline, match)) {
+			if (strcmp(&(oneline[match[1].rm_so]), "FIRMWARE_LOCATE") == 0) {
+				continue;
+			} else {
+				path_filename = malloc(MAX_LINE_SIZE);
+				strcpy(path_filename, dir);
+				strcat(path_filename, "/");
+				strcat(path_filename, &(oneline[match[2].rm_so]));
+
+				if (find_register_fw_filename_psp_dir(
+						&(oneline[match[1].rm_so]),
+						path_filename, cb_config) == 0) {
+					if (find_register_fw_filename_bios_dir(
+							&(oneline[match[1].rm_so]),
+							path_filename, cb_config) == 0) {
+						fprintf(stderr, "Module's name \"%s\" is not valid\n", oneline);
+						return 0; /* Stop parsing. */
+					} else {
+						if (print_deps)
+							printf(" %s ", path_filename);
+					}
+				} else {
+					if (print_deps)
+						printf(" %s ", path_filename);
+				}
+			}
+		} else {
+			fprintf(stderr, "AMDFWTOOL config file line can't be parsed \"%s\"\n", oneline);
+			return 0;
+		}
+	}
+	return 1;
+}

src/soc/amd/picasso/Kconfig
@@ -446,9 +446,9 @@ comment "AMD Firmware Directory Table set to location for 8MB ROM"
 comment "AMD Firmware Directory Table set to location for 16MB ROM"
 	depends on AMD_FWM_POSITION_INDEX = 5
 
-config AMD_PUBKEY_FILE
+config AMDFW_CONFIG_FILE
 	string
-	default "3rdparty/amd_blobs/picasso/PSP/AmdPubKeyRV.bin"
+	default "src/soc/amd/picasso/fw.cfg"
 
 config USE_PSPSECUREOS
 	bool
@@ -486,16 +486,6 @@ config PSP_WHITELIST_FILE
 	string "Debug whitelist file path"
 	depends on HAVE_PSP_WHITELIST_FILE
 	default "3rdparty/amd_blobs/picasso/PSP/wtl-rvn.sbin"
-
-config PSP_BOOTLOADER_FILE
-	string "Specify the PSP Bootloader file path"
-	default "3rdparty/amd_blobs/picasso/PSP/PspBootLoader_WL_RV.sbin" if HAVE_PSP_WHITELIST_FILE
-	default "3rdparty/amd_blobs/picasso/PSP/PspBootLoader_prod_RV.sbin"
-	help
-	  Supply the name of the PSP bootloader file.
-
-	  Note that this option may conflict with the whitelist file if a
-	  different PSP bootloader binary is specified.
 
 config PSP_SHAREDMEM_SIZE
 	hex "Maximum size of shared memory area"

src/soc/amd/picasso/Makefile.inc
@@ -125,77 +125,35 @@ PICASSO_FWM_POSITION=$(call int-add, \
 # Design Guide for AMD Family 17h Processors" (PID #55758, NDA only).
 #
 
-# type = 0x0
-FIRMWARE_LOCATE=$(realpath $(dir $(call strip_quotes, $(CONFIG_AMD_PUBKEY_FILE))))
-
-# type = 0x1
-ifeq ($(CONFIG_PSP_BOOTLOADER_FILE),)
-$(error CONFIG_PSP_BOOTLOADER_FILE was not defined)
-endif
-PSPBTLDR_FILE=$(realpath $(call strip_quotes, $(CONFIG_PSP_BOOTLOADER_FILE)))
-$(info Adding PSP $(shell dd if=$(PSPBTLDR_FILE) | md5sum))
-
-# types = 0x8 and 0x12
-PSP_SMUFW1_SUB1_FILE=$(FIRMWARE_LOCATE)/SmuFirmwareRV2.csbin
-PSP_SMUFW1_SUB2_FILE=$(FIRMWARE_LOCATE)/SmuFirmwarePCO.csbin
-PSP_SMUFW2_SUB1_FILE=$(FIRMWARE_LOCATE)/SmuFirmware2RV2.csbin
-PSP_SMUFW2_SUB2_FILE=$(FIRMWARE_LOCATE)/SmuFirmware2PCO.csbin
+FIRMWARE_LOCATE=$(shell grep -e FIRMWARE_LOCATE $(CONFIG_AMDFW_CONFIG_FILE) | awk '{print $$2}')
 
 ifeq ($(CONFIG_PSP_UNLOCK_SECURE_DEBUG),y)
-# type = 0x9
-PSP_SEC_DBG_KEY_FILE=$(FIRMWARE_LOCATE)/RavenSecureDebug_PublicKey.bin
-# type = 0x13
-PSP_SEC_DEBUG_FILE=$(FIRMWARE_LOCATE)/secure_unlock_prod_RV.sbin
 # Enable secure debug unlock
 PSP_SOFTFUSE_BITS += 0
-PSP_TOKEN_UNLOCK="--token-unlock"
+OPT_TOKEN_UNLOCK="--token-unlock"
 endif
 
 ifeq ($(CONFIG_USE_PSPSECUREOS),y)
 # types = 0x2
-PSPSECUREOS_FILE=$(FIRMWARE_LOCATE)/psp_os_combined_prod_RV.sbin
+OPT_PSP_USE_PSPSECUREOS="--use-pspsecureos"
 endif
 
-# type = 0x21
-PSP_IKEK_FILE=$(FIRMWARE_LOCATE)/PspIkekRV.bin
-
-# type = 0x24
-PSP_SECG1_FILE=$(FIRMWARE_LOCATE)/security_policy_RV2_FP5_AM4.sbin
-PSP_SECG2_FILE=$(FIRMWARE_LOCATE)/security_policy_PCO_FP5_AM4.sbin
 
 ifeq ($(CONFIG_PSP_LOAD_MP2_FW),y)
-# type = 0x25
-PSP_MP2FW1_FILE=$(FIRMWARE_LOCATE)/MP2I2CFWRV2.sbin
-PSP_MP2FW2_FILE=$(FIRMWARE_LOCATE)/MP2I2CFWPCO.sbin
-# BIOS type = 0x6a
-PSP_MP2CFG_FILE=$(FIRMWARE_LOCATE)/MP2FWConfig.sbin
+OPT_PSP_LOAD_MP2_FW="--load-mp2-fw"
 else
 # Disable MP2 firmware loading
 PSP_SOFTFUSE_BITS += 29
 endif
 
-# type = 0x28
-PSP_DRIVERS_FILE=$(FIRMWARE_LOCATE)/drv_sys_prod_RV.sbin
-
 ifeq ($(CONFIG_PSP_LOAD_S0I3_FW),y)
-PSP_S0I3_FILE=$(FIRMWARE_LOCATE)/dr_agesa_prod_RV.sbin
+OPT_PSP_LOAD_S0I3_FW="--load-s0i3"
 endif
-
-# types = 0x30 - 0x37
-PSP_ABL0_FILE=$(FIRMWARE_LOCATE)/AgesaBootloader0_prod_RV.csbin
-PSP_ABL1_FILE=$(FIRMWARE_LOCATE)/AgesaBootloader1_prod_RV.csbin
-PSP_ABL2_FILE=$(FIRMWARE_LOCATE)/AgesaBootloader2_prod_RV.csbin
-PSP_ABL3_FILE=$(FIRMWARE_LOCATE)/AgesaBootloader3_prod_RV.csbin
-PSP_ABL4_FILE=$(FIRMWARE_LOCATE)/AgesaBootloader4_prod_RV.csbin
-PSP_ABL5_FILE=$(FIRMWARE_LOCATE)/AgesaBootloader5_prod_RV.csbin
-PSP_ABL6_FILE=$(FIRMWARE_LOCATE)/AgesaBootloader6_prod_RV.csbin
-PSP_ABL7_FILE=$(FIRMWARE_LOCATE)/AgesaBootloader7_prod_RV.csbin
 
 # type = 0x3a
 ifeq ($(CONFIG_HAVE_PSP_WHITELIST_FILE),y)
 PSP_WHITELIST_FILE=$(CONFIG_PSP_WHITELIST_FILE)
 endif
-
 #
 # BIOS Directory Table items - proper ordering is managed by amdfwtool
 #
@@ -215,16 +173,6 @@ PSP_BIOSBIN_DEST=$(shell $(READELF_bootblock) -l $(PSP_ELF_FILE) | grep LOAD | a
 # The flashmap section used for this is expected to be named RW_MRC_CACHE
 APOB_NV_SIZE=$(shell grep "FMAP_SECTION_RW_MRC_CACHE_SIZE" $(obj)/fmap_config.h | awk '{print $$(NF)}')
 APOB_NV_BASE=$(shell grep "FMAP_SECTION_RW_MRC_CACHE_START" $(obj)/fmap_config.h | awk '{print $$(NF)}')
-
-# type2 = 0x64, 0x65
-PSP_PMUI_FILE1=$(FIRMWARE_LOCATE)/Appb_Rv_1D_Ddr4_Udimm_Imem.csbin
-PSP_PMUI_FILE2=$(FIRMWARE_LOCATE)/Appb_Rv_2D_Ddr4_Imem.csbin
-PSP_PMUI_FILE3=$(FIRMWARE_LOCATE)/Appb_Rv2_1D_ddr4_Udimm_Imem.csbin
-PSP_PMUI_FILE4=$(FIRMWARE_LOCATE)/Appb_Rv2_2D_ddr4_Udimm_Imem.csbin
-PSP_PMUD_FILE1=$(FIRMWARE_LOCATE)/Appb_Rv_1D_Ddr4_Udimm_Dmem.csbin
-PSP_PMUD_FILE2=$(FIRMWARE_LOCATE)/Appb_Rv_2D_Ddr4_Dmem.csbin
-PSP_PMUD_FILE3=$(FIRMWARE_LOCATE)/Appb_Rv2_1D_ddr4_Udimm_Dmem.csbin
-PSP_PMUD_FILE4=$(FIRMWARE_LOCATE)/Appb_Rv2_2D_ddr4_Udimm_Dmem.csbin
 
 # type = 0x66
 PSP_UCODE_FILE1=$(FIRMWARE_LOCATE)/UcodePatch_PCO_B1.bin
@@ -259,33 +207,6 @@ PSP_SOFTFUSE=$(shell A=$(call int-add, \
 
 add_opt_prefix=$(if $(call strip_quotes, $(1)), $(2) $(call strip_quotes, $(1)), )
 
-OPT_AMD_PUBKEY_FILE=$(call add_opt_prefix, $(CONFIG_AMD_PUBKEY_FILE), --pubkey)
-OPT_PSPBTLDR_FILE=$(call add_opt_prefix, $(PSPBTLDR_FILE), --bootloader)
-OPT_SMUFW1_SUB1_FILE=$(call add_opt_prefix, $(PSP_SMUFW1_SUB1_FILE), --subprogram 1 --smufirmware)
-OPT_SMUFW1_SUB2_FILE=$(call add_opt_prefix, $(PSP_SMUFW1_SUB2_FILE), --subprogram 2 --smufirmware)
-OPT_SMUFW2_SUB1_FILE=$(call add_opt_prefix, $(PSP_SMUFW2_SUB1_FILE), --subprogram 1 --smufirmware2)
-OPT_SMUFW2_SUB2_FILE=$(call add_opt_prefix, $(PSP_SMUFW2_SUB2_FILE), --subprogram 2 --smufirmware2)
-OPT_PSP_SEC_DBG_KEY_FILE=$(call add_opt_prefix, $(PSP_SEC_DBG_KEY_FILE), --securedebug)
-OPT_TOKEN_UNLOCK=$(call add_opt_prefix, $(PSP_TOKEN_UNLOCK), "")
-OPT_PSP_SOFTFUSE=$(call add_opt_prefix, $(PSP_SOFTFUSE), --soft-fuse)
-OPT_PSPSECUREOS_FILE=$(call add_opt_prefix, $(PSPSECUREOS_FILE), --secureos)
-OPT_SEC_DEBUG_FILE=$(call add_opt_prefix, $(PSP_SEC_DEBUG_FILE), --secdebug)
-OPT_IKEK_FILE=$(call add_opt_prefix, $(PSP_IKEK_FILE), --ikek)
-OPT_SECG1_FILE=$(call add_opt_prefix, $(PSP_SECG1_FILE), --subprog 1 --sec-gasket)
-OPT_SECG2_FILE=$(call add_opt_prefix, $(PSP_SECG2_FILE), --subprog 2 --sec-gasket)
-OPT_MP2FW1_FILE=$(call add_opt_prefix, $(PSP_MP2FW1_FILE), --subprog 1 --mp2-fw)
-OPT_MP2FW2_FILE=$(call add_opt_prefix, $(PSP_MP2FW2_FILE), --subprog 2 --mp2-fw)
-OPT_DRIVERS_FILE=$(call add_opt_prefix, $(PSP_DRIVERS_FILE), --drv-entry-pts)
-OPT_PSP_S0I3_FILE=$(call add_opt_prefix, $(PSP_S0I3_FILE), --s0i3drv)
-OPT_ABL0_FILE=$(call add_opt_prefix,  $(PSP_ABL0_FILE), --abl-image)
-OPT_ABL1_FILE=$(call add_opt_prefix,  $(PSP_ABL1_FILE), --abl-image)
-OPT_ABL2_FILE=$(call add_opt_prefix,  $(PSP_ABL2_FILE), --abl-image)
-OPT_ABL3_FILE=$(call add_opt_prefix,  $(PSP_ABL3_FILE), --abl-image)
-OPT_ABL4_FILE=$(call add_opt_prefix,  $(PSP_ABL4_FILE), --abl-image)
-OPT_ABL5_FILE=$(call add_opt_prefix,  $(PSP_ABL5_FILE), --abl-image)
-OPT_ABL6_FILE=$(call add_opt_prefix,  $(PSP_ABL6_FILE), --abl-image)
-OPT_ABL7_FILE=$(call add_opt_prefix,  $(PSP_ABL7_FILE), --abl-image)
-OPT_WHITELIST_FILE=$(call add_opt_prefix, $(PSP_WHITELIST_FILE), --whitelist)
 OPT_VERSTAGE_FILE=$(call add_opt_prefix, $(PSP_VERSTAGE_FILE), --verstage)
 OPT_VERSTAGE_SIG_FILE=$(call add_opt_prefix, $(PSP_VERSTAGE_SIG_FILE), --verstage_sig)
 
@@ -297,15 +218,7 @@ OPT_APOB_ADDR=$(call add_opt_prefix, $(PSP_APOB_BASE), --apob-base)
 OPT_PSP_BIOSBIN_FILE=$(call add_opt_prefix, $(PSP_BIOSBIN_FILE), --bios-bin)
 OPT_PSP_BIOSBIN_DEST=$(call add_opt_prefix, $(PSP_BIOSBIN_DEST), --bios-bin-dest)
 OPT_PSP_BIOSBIN_SIZE=$(call add_opt_prefix, $(PSP_BIOSBIN_SIZE), --bios-uncomp-size)
-OPT_PSP_PMUI_FILE1=$(call add_opt_prefix, $(PSP_PMUI_FILE1), --subprogram 0 --instance 1 --pmu-inst)
-OPT_PSP_PMUI_FILE2=$(call add_opt_prefix, $(PSP_PMUI_FILE2), --subprogram 0 --instance 4 --pmu-inst)
-OPT_PSP_PMUI_FILE3=$(call add_opt_prefix, $(PSP_PMUI_FILE3), --subprogram 1 --instance 1 --pmu-inst)
-OPT_PSP_PMUI_FILE4=$(call add_opt_prefix, $(PSP_PMUI_FILE4), --subprogram 1 --instance 4 --pmu-inst)
-OPT_PSP_PMUD_FILE1=$(call add_opt_prefix, $(PSP_PMUD_FILE1), --subprogram 0 --instance 1 --pmu-data)
-OPT_PSP_PMUD_FILE2=$(call add_opt_prefix, $(PSP_PMUD_FILE2), --subprogram 0 --instance 4 --pmu-data)
-OPT_PSP_PMUD_FILE3=$(call add_opt_prefix, $(PSP_PMUD_FILE3), --subprogram 1 --instance 1 --pmu-data)
-OPT_PSP_PMUD_FILE4=$(call add_opt_prefix, $(PSP_PMUD_FILE4), --subprogram 1 --instance 4 --pmu-data)
-OPT_MP2CFG_FILE=$(call add_opt_prefix, $(PSP_MP2CFG_FILE), --mp2-config)
+
 OPT_PSP_SHAREDMEM_BASE=$(call add_opt_prefix, $(PSP_SHAREDMEM_BASE), --sharedmem)
 OPT_PSP_SHAREDMEM_SIZE=$(call add_opt_prefix, $(PSP_SHAREDMEM_SIZE), --sharedmem-size)
 OPT_APOB_NV_SIZE=$(call add_opt_prefix, $(APOB_NV_SIZE), --apob-nv-size)
@@ -314,49 +227,28 @@ OPT_EFS_SPI_READ_MODE=$(call add_opt_prefix, $(CONFIG_EFS_SPI_READ_MODE), --spi-
 OPT_EFS_SPI_SPEED=$(call add_opt_prefix, $(CONFIG_EFS_SPI_SPEED), --spi-speed)
 OPT_EFS_SPI_MICRON_FLAG=$(call add_opt_prefix, $(CONFIG_EFS_SPI_MICRON_FLAG), --spi-micron-flag)
 
+OPT_PSP_SOFTFUSE=$(call add_opt_prefix, $(PSP_SOFTFUSE), --soft-fuse)
+
 ifeq ($(CONFIG_VBOOT),)
 OPT_APOB0_NV_SIZE=$(OPT_APOB_NV_SIZE)
 OPT_APOB0_NV_BASE=$(OPT_APOB_NV_BASE)
 endif
 
-AMDFW_COMMON_ARGS=$(OPT_AMD_PUBKEY_FILE) \
-		$(OPT_PSPSECUREOS_FILE) \
-		$(OPT_PSP_SEC_DBG_KEY_FILE) \
-		$(OPT_SMUFW1_SUB2_FILE) \
-		$(OPT_SMUFW2_SUB2_FILE) \
-		$(OPT_SMUFW1_SUB1_FILE) \
-		$(OPT_SMUFW2_SUB1_FILE) \
-		$(OPT_PSP_APCB_FILES) \
+OPT_WHITELIST_FILE=$(call add_opt_prefix, $(PSP_WHITELIST_FILE), --whitelist)
+
+# Add all the files listed in the config file
+DEP_FILES=$(shell $(AMDFWTOOL) --config $(CONFIG_AMDFW_CONFIG_FILE) --depend)
+
+AMDFW_COMMON_ARGS=$(OPT_PSP_APCB_FILES) \
 		$(OPT_APOB_ADDR) \
 		$(OPT_PSP_BIOSBIN_FILE) \
 		$(OPT_PSP_BIOSBIN_DEST) \
 		$(OPT_PSP_BIOSBIN_SIZE) \
 		$(OPT_PSP_SOFTFUSE) \
-		$(OPT_PSP_PMUI_FILE1) \
-		$(OPT_PSP_PMUI_FILE2) \
-		$(OPT_PSP_PMUI_FILE3) \
-		$(OPT_PSP_PMUI_FILE4) \
-		$(OPT_PSP_PMUD_FILE1) \
-		$(OPT_PSP_PMUD_FILE2) \
-		$(OPT_PSP_PMUD_FILE3) \
-		$(OPT_PSP_PMUD_FILE4) \
-		$(OPT_MP2CFG_FILE) \
-		$(OPT_ABL0_FILE) \
-		$(OPT_ABL1_FILE) \
-		$(OPT_ABL2_FILE) \
-		$(OPT_ABL3_FILE) \
-		$(OPT_ABL4_FILE) \
-		$(OPT_ABL5_FILE) \
-		$(OPT_ABL6_FILE) \
-		$(OPT_ABL7_FILE) \
+		$(OPT_PSP_USE_PSPSECUREOS) \
+		$(OPT_PSP_LOAD_MP2_FW) \
+		$(OPT_PSP_LOAD_S0I3_FW) \
 		$(OPT_WHITELIST_FILE) \
-		$(OPT_SECG1_FILE) \
-		$(OPT_SECG2_FILE) \
-		$(OPT_MP2FW1_FILE) \
-		$(OPT_MP2FW2_FILE) \
-		$(OPT_DRIVERS_FILE) \
-		$(OPT_PSP_S0I3_FILE) \
-		$(OPT_IKEK_FILE) \
 		$(OPT_SEC_DEBUG_FILE) \
 		$(OPT_PSP_SHAREDMEM_BASE) \
 		$(OPT_PSP_SHAREDMEM_SIZE) \
@@ -365,45 +257,15 @@ AMDFW_COMMON_ARGS=$(OPT_AMD_PUBKEY_FILE) \
 		$(OPT_EFS_SPI_READ_MODE) \
 		$(OPT_EFS_SPI_SPEED) \
 		$(OPT_EFS_SPI_MICRON_FLAG) \
+		--config $(CONFIG_AMDFW_CONFIG_FILE) \
 		--soc-name "Picasso" \
 		--flashsize $(CONFIG_ROM_SIZE)
 
-$(obj)/amdfw.rom:	$(call strip_quotes, $(CONFIG_AMD_PUBKEY_FILE)) \
-			$(call strip_quotes, $(PSPBTLDR_FILE)) \
-			$(call strip_quotes, $(PSPSECUREOS_FILE)) \
-			$(call strip_quotes, $(PSP_SEC_DBG_KEY_FILE)) \
-			$(call strip_quotes, $(PSP_BIOSBIN_FILE)) \
-			$(call strip_quotes, $(PSP_PMUI_FILE1)) \
-			$(call strip_quotes, $(PSP_PMUI_FILE2)) \
-			$(call strip_quotes, $(PSP_PMUI_FILE3)) \
-			$(call strip_quotes, $(PSP_PMUI_FILE4)) \
-			$(call strip_quotes, $(PSP_PMUD_FILE1)) \
-			$(call strip_quotes, $(PSP_PMUD_FILE2)) \
-			$(call strip_quotes, $(PSP_PMUD_FILE3)) \
-			$(call strip_quotes, $(PSP_PMUD_FILE4)) \
-			$(call strip_quotes, $(PSP_MP2CFG_FILE)) \
-			$(call strip_quotes, $(PSP_SMUFW1_SUB1_FILE)) \
-			$(call strip_quotes, $(PSP_SMUFW1_SUB2_FILE)) \
-			$(call strip_quotes, $(PSP_SMUFW2_SUB1_FILE)) \
-			$(call strip_quotes, $(PSP_SMUFW2_SUB2_FILE)) \
-			$(call strip_quotes, $(PSP_ABL0_FILE)) \
-			$(call strip_quotes, $(PSP_ABL1_FILE)) \
-			$(call strip_quotes, $(PSP_ABL2_FILE)) \
-			$(call strip_quotes, $(PSP_ABL3_FILE)) \
-			$(call strip_quotes, $(PSP_ABL4_FILE)) \
-			$(call strip_quotes, $(PSP_ABL5_FILE)) \
-			$(call strip_quotes, $(PSP_ABL6_FILE)) \
-			$(call strip_quotes, $(PSP_ABL7_FILE)) \
-			$(call strip_quotes, $(PSP_WHITELIST_FILE)) \
-			$(call strip_quotes, $(PSP_SECG1_FILE)) \
-			$(call strip_quotes, $(PSP_SECG2_FILE)) \
-			$(call_strip_quotes, $(PSP_DRIVERS_FILE)) \
-			$(call_strip_quotes, $(PSP_S0I3_FILE)) \
-			$(call_strip_quotes, $(PSP_IKEK_FILE)) \
-			$(call_strip_quotes, $(PSP_SEC_DEBUG_FILE)) \
+$(obj)/amdfw.rom:	$(call strip_quotes, $(PSP_BIOSBIN_FILE)) \
 			$(PSP_VERSTAGE_FILE) \
 			$(PSP_VERSTAGE_SIG_FILE) \
 			$$(PSP_APCB_FILES) \
+			$(DEP_FILES) \
 			$(AMDFWTOOL) \
 			$(obj)/fmap_config.h
 	$(if $(PSP_APCB_FILES), ,$(error APCB_SOURCES is not set))

src/soc/amd/picasso/fw.cfg
@@ -1,0 +1,39 @@
+# PSP fw config file
+
+FIRMWARE_LOCATE        3rdparty/amd_blobs/picasso/PSP
+
+# type                    file
+AMD_PUBKEY_FILE        AmdPubKeyRV.bin
+PSPBTLDR_FILE          PspBootLoader_prod_RV.sbin
+PSPBTLDR_WL_FILE       PspBootLoader_WL_RV.sbin
+PSP_SMUFW1_SUB1_FILE   SmuFirmwareRV2.csbin
+PSP_SMUFW1_SUB2_FILE   SmuFirmwarePCO.csbin
+PSP_SMUFW2_SUB1_FILE   SmuFirmware2RV2.csbin
+PSP_SMUFW2_SUB2_FILE   SmuFirmware2PCO.csbin
+PSPSECUREOS_FILE       psp_os_combined_prod_RV.sbin
+PSP_SEC_DBG_KEY_FILE   RavenSecureDebug_PublicKey.bin
+PSP_SEC_DEBUG_FILE     secure_unlock_prod_RV.sbin
+PSP_ABL0_FILE          AgesaBootloader0_prod_RV.csbin
+PSP_ABL1_FILE          AgesaBootloader1_prod_RV.csbin
+PSP_ABL2_FILE          AgesaBootloader2_prod_RV.csbin
+PSP_ABL3_FILE          AgesaBootloader3_prod_RV.csbin
+PSP_ABL4_FILE          AgesaBootloader4_prod_RV.csbin
+PSP_ABL5_FILE          AgesaBootloader5_prod_RV.csbin
+PSP_ABL6_FILE          AgesaBootloader6_prod_RV.csbin
+PSP_ABL7_FILE          AgesaBootloader7_prod_RV.csbin
+PSP_IKEK_FILE          PspIkekRV.bin
+PSP_SECG1_FILE         security_policy_RV2_FP5_AM4.sbin
+PSP_SECG2_FILE         security_policy_PCO_FP5_AM4.sbin
+PSP_MP2FW1_FILE        MP2I2CFWRV2.sbin
+PSP_MP2FW2_FILE        MP2I2CFWPCO.sbin
+PSP_MP2CFG_FILE        MP2FWConfig.sbin
+PSP_DRIVERS_FILE       drv_sys_prod_RV.sbin
+# BDT
+PSP_PMUI_FILE1         Appb_Rv_1D_Ddr4_Udimm_Imem.csbin
+PSP_PMUI_FILE2         Appb_Rv_2D_Ddr4_Imem.csbin
+PSP_PMUI_FILE3         Appb_Rv2_1D_ddr4_Udimm_Imem.csbin
+PSP_PMUI_FILE4         Appb_Rv2_2D_ddr4_Udimm_Imem.csbin
+PSP_PMUD_FILE1         Appb_Rv_1D_Ddr4_Udimm_Dmem.csbin
+PSP_PMUD_FILE2         Appb_Rv_2D_Ddr4_Dmem.csbin
+PSP_PMUD_FILE3         Appb_Rv2_1D_ddr4_Udimm_Dmem.csbin
+PSP_PMUD_FILE4         Appb_Rv2_2D_ddr4_Udimm_Dmem.csbin

src/soc/amd/stoneyridge/Kconfig
@@ -190,12 +190,13 @@ config STONEYRIDGE_GEC_FWM_FILE
 	string "GEC firmware path and filename"
 	depends on STONEYRIDGE_GEC_FWM
 
-config AMD_PUBKEY_FILE
-	string "AMD public Key"
+config AMDFW_CONFIG_FILE
+	string
+	string "AMD PSP Firmware config file"
 	default "" if !USE_AMD_BLOBS
-	default "3rdparty/amd_blobs/stoneyridge/PSP/CZ/AmdPubKeyCZ.bin" if AMD_APU_MERLINFALCON
-	default "3rdparty/amd_blobs/stoneyridge/PSP/ST/AmdPubKeyST.bin" if AMD_APU_PRAIRIEFALCON
-	default "3rdparty/amd_blobs/stoneyridge/PSP/ST/AmdPubKeyST.bin" if AMD_APU_STONEYRIDGE
+	default "src/soc/amd/stoneyridge/fw_cz.cfg" if AMD_APU_MERLINFALCON
+	default "src/soc/amd/stoneyridge/fw_st.cfg" if AMD_APU_PRAIRIEFALCON
+	default "src/soc/amd/stoneyridge/fw_st.cfg" if AMD_APU_STONEYRIDGE
 
 config STONEYRIDGE_SATA_MODE
 	int "SATA Mode"

commit 577e723f699e68e424d1587ff396ea659e53d3d3

util/amdfwtool: Fix regression for PSPTRUSTLETS_FILE
Fix regression introduced in commit c5e28abaf803465ae4bfec1904618497e077ca50
"amdfwtool: Take a config file instead of command line parameters".

PSPTRUSTLETS_FILE is twice in the list, making the second code path
unreachable. On Stoneyridge the file is now always added to the PSP
directory, even when CONFIG_USE_PSPSECUREOS=n. On other platforms the
file is always added, thus it was never noticed.

Drop the first occurence and only include it when use-pspsecureos is
specified on the commandline.

TEST=Untested.

Change-Id: I54f4af84d50d6ed7abb550ca9e04774f0811eae3
Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/92569
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
Reviewed-by: Andy Ebrahiem <ahmet.ebrahiem@9elements.com>
Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

util/amdfwtool/data_parse.c
@@ -163,9 +163,6 @@ static uint8_t find_register_fw_filename_psp_dir(char *fw_name, char *filename,
 	} else if (strcmp(fw_name, "SMUSCS_FILE") == 0) {
 		fw_type = AMD_FW_PSP_SMUSCS;
 		subprog = 0;
-	} else if (strcmp(fw_name, "PSPTRUSTLETS_FILE") == 0) {
-		fw_type = AMD_FW_PSP_TRUSTLETS;
-		subprog = 0;
 	} else if (strcmp(fw_name, "PSPSECUREDEBUG_FILE") == 0) {
 		fw_type = AMD_FW_PSP_SECURED_DEBUG;
 		subprog = 0;