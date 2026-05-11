02f4c943da2879d9aabd4add4388cc10cc6f0a36
Luca Lai <luca.lai@lcfc.corp-partner.google.com>
2026-05-11 15:11:03 +0800

    mb/google/nissa/var/pujjolo: Add FW_CONFIG probe for WWAN devices
    Add FW_CONFIG probe based on pujjolo boxster of below devices:
    WWAN.The SOC_I2C_SUB_INT_ODL is the interrupt pin of p-sensor.
    So this pin should be set to NC when the sku is non-wwan sku.
    
    BUG=b:487952755
    TEST=Boot to OS and verify the WWAN devices is set based on fw_config using below methods
    1. cpu log show [INFO] Disable WWAN-related GPIO pins.
    2. EE team use oscilloscope to check GPIO pin's status.
    
    Change-Id: I7b88554303b2d87c2b04d6abf7fe85e48f5c483e
    Signed-off-by: Luca Lai <luca.lai@lcfc.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92615
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/pujjolo/fw_config.c
+++ src/mainboard/google/brya/variants/pujjolo/fw_config.c
@@ -16,11 +16,38 @@ static const struct pad_config ish_disable_pads[] = {
 	PAD_NC_LOCK(GPP_B6, NONE, LOCK_CONFIG),
 };
 
+static const struct pad_config wwan_disable_pads[] = {
+	/* A8  : WWAN_RF_DISABLE_ODL */
+	PAD_NC(GPP_A8, NONE),
+	/* B8  : WWAN_SAR_DETECT_2_ODL */
+	PAD_NC(GPP_B8, NONE),
+	/* D5  : SRCCLKREQ0# ==> WWAN_CLKREQ_ODL */
+	PAD_NC(GPP_D5, NONE),
+	/* D6  : WWAN_EN */
+	PAD_NC(GPP_D6, NONE),
+	/* E16 : WWAN_PCIE_WAKE_ODL */
+	PAD_NC(GPP_E16, NONE),
+	/* E17 : WWAN_RST_L */
+	PAD_NC_LOCK(GPP_E17, NONE, LOCK_CONFIG),
+	/* H19 : SOC_I2C_SUB_INT_ODL */
+	PAD_NC(GPP_H19, NONE),
+	/* H21  : WCAM_MCLK_R ==> WWAN_PERST_L */
+	PAD_NC_LOCK(GPP_H21, NONE, LOCK_CONFIG),
+	/* H23 : WWAN_SAR_DETECT_ODL */
+	PAD_NC(GPP_H23, NONE),
+};
+
 void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
 {
 	if (fw_config_probe(FW_CONFIG(TABLET_MODE, TABLET_MODE_DISABLE))) {
 		printk(BIOS_INFO, "Disable ISH-related GPIO pins.\n");
 		gpio_padbased_override(padbased_table, ish_disable_pads,
 						ARRAY_SIZE(ish_disable_pads));
+	}
+
+	if (fw_config_probe(FW_CONFIG(WWAN, WWAN_ABSENT))) {
+		printk(BIOS_INFO, "Disable WWAN-related GPIO pins.\n");
+		gpio_padbased_override(padbased_table, wwan_disable_pads,
+						ARRAY_SIZE(wwan_disable_pads));
 	}
 }
